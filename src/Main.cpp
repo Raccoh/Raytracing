e31facbd62795579dd24def9274bedac75f15167
Nico Zimmermann <nicotom.zim@gmail.com>
2017-08-20 10:07:25 +0200

    Better types

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,26 +1,14 @@
-#include <cmath>
 #include <cstdio>
 
-struct vec3d {
-  double x, y, z;
-  vec3d(double x_=0, double y_=0, double z_=0){ x=x_; y=y_; z=z_; }
-  vec3d operator+(const vec3d &b) const { return vec3d(x+b.x,y+b.y,z+b.z); }
-  vec3d operator-(const vec3d &b) const { return vec3d(x-b.x,y-b.y,z-b.z); }
-  vec3d operator*(double b) const { return vec3d(x*b,y*b,z*b); }
-  vec3d mult(const vec3d &b) const { return vec3d(x*b.x,y*b.y,z*b.z); }
-  vec3d& norm(){ return *this = *this * (1/sqrt(x*x+y*y+z*z)); }
-  double dot(const vec3d &b) const { return x*b.x+y*b.y+z*b.z; }
-  vec3d operator%(vec3d&b){return vec3d(y*b.z-z*b.y,z*b.x-x*b.z,x*b.y-y*b.x);}
-};
+#include "Types.h"
 
-void writeImage(vec3d* pixels, int width, int height) {
+void writeImage(color4i* pixels, int width, int height) {
   FILE* f = fopen("image.ppm", "w");
   fprintf(f, "P3\n%d %d\n%d\n", width, height, 255);
   for (int row = 0; row < height; ++row) {
     for (int col = 0; col < width; ++col) {
       int i = row * width + col;
-      fprintf(f, "%d %d %d ",
-              (int)pixels[i].x, (int)pixels[i].y, (int)pixels[i].z);
+      fprintf(f, "%d %d %d ", pixels[i].r, pixels[i].g, pixels[i].b);
     }
   }
   fclose(f);
@@ -30,10 +18,10 @@ int main()
 {
   // Create an image with only blue pixels.
   int w = 1024, h = 768;
-  vec3d* pixels = new vec3d[w * h];
+  color4i* pixels = new color4i[w * h];
   for (int row = 0; row < h; ++row) {
     for (int col = 0; col < w; ++col) {
-      pixels[row * w + col] = vec3d(0, 0, 255);
+      pixels[row * w + col] = color4i(0, 0, 255);
     }
   }
   writeImage(pixels, w, h);
---
+++ src/Types.h
@@ -0,0 +1,40 @@
+#include <cmath>
+#include <algorithm>
+
+struct vec3d {
+  double x = 0, y = 0, z = 0;
+  vec3d(double x_, double y_, double z_) {
+    x = x_; y = y_; z = z_;
+  }
+  vec3d operator+(const vec3d& b) const { return vec3d(x+b.x,y+b.y,z+b.z); }
+  vec3d operator-(const vec3d& b) const { return vec3d(x-b.x,y-b.y,z-b.z); }
+  vec3d operator*(double b) const { return vec3d(x*b,y*b,z*b); }
+  vec3d mult(const vec3d& b) const { return vec3d(x*b.x,y*b.y,z*b.z); }
+  vec3d& norm() { return *this = *this * (1/sqrt(x*x+y*y+z*z)); }
+  double dot(const vec3d& b) const { return x*b.x+y*b.y+z*b.z; }
+  vec3d cross(const vec3d& b) {
+    return vec3d(y*b.z-z*b.y,z*b.x-x*b.z,x*b.y-y*b.x);
+  }
+};
+
+struct color4i {
+  int r = 0, g = 0, b = 0, a = 255;
+  color4i() {}
+  color4i(int r_, int g_, int b_, int a_ = 255) {
+    r_ = std::max(0, std::min(255, r_));
+    g_ = std::max(0, std::min(255, g_));
+    b_ = std::max(0, std::min(255, b_));
+    a_ = std::max(0, std::min(255, a_));
+    r = r_; g = g_; b = b_; a = a_;
+  }
+  color4i(double r_, double g_, double b_, double a_ = 255) {
+    color4i((int)(r_ * 255 + 0.5), (int)(g_ * 255 + 0.5),
+            (int)(b_ * 255 + 0.5), (int)(a_ * 255 + 0.5));
+  }
+  color4i operator+(const color4i& c) const {
+    return color4i(r+c.r, g+c.g, b+c.b, a+c.a);
+  }
+  color4i operator-(const color4i& c) const {
+    return color4i(r-c.r, g-c.g, b-c.b, a-c.a);
+  }
+};
